4f9aaade03426e14ae4c8f5e664905b7717f1334
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2023-07-20 12:12:45 +0900

    급하게 커밋

--- src/main/java/com/jgj/byl_process/domain/config/WebConfig.java
+++ src/main/java/com/jgj/byl_process/domain/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://127.0.0.1:8080", "http://localhost:8080", "http://15.165.2.78")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
