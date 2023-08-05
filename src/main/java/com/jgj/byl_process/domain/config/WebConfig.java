ec2d4dd21c47fa177b8edad280f97c2262488b69
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2023-08-05 19:31:05 +0900

    add: CORS 규칙 추가

--- src/main/java/com/jgj/byl_process/domain/config/WebConfig.java
+++ src/main/java/com/jgj/byl_process/domain/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:8080", "http://localhost:8080", "http://15.165.2.78")
+                .allowedOrigins("http://127.0.0.1:8080", "http://localhost:8080", "http://15.165.2.78", "http://ec2-15-165-2-78.ap-northeast-2.compute.amazonaws.com", "http://byl.kro.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
